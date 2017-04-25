15921e27edc338ddedc6d9872124347e611f5beb
Nigel Said <nigelsaid@gmail.com>
2017-04-25 22:10:38 +0100

    enters player names

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Nigel'
+    fill_in :player_2_name, with: 'Peter'
+    click_button 'Submit'
+    expect(page).to have_content 'Nigel vs. Peter'
+  end
+end
